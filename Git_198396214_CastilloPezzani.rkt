768d8f3b26335eadaeb03304f1e90feaa9f49f78
Aldo <Hola564@hotmail.es>
2020-05-10 17:56:23 -0400

    Creacion archivo base

---
+++ Git_198396214_CastilloPezzani.rkt
@@ -0,0 +1,17 @@
+#!r6rs
+
+;Aqui se importa r6rs
+(import (rnrs lists (6))
+        (rnrs base (6))
+        (rnrs io simple (6))
+        (rnrs r5rs (6)))
+
+
+;########################################################################################
+
+
+;TDA Archivo
+; nombre
+; contenido
+; lineas
+; numero de caracteres
